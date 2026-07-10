6b5a8503b3927c94b3219ded037a79e529d83b91
@Dendroid01 <denis.gordienko06@gmail.com>
2026-07-10 14:49:46 +0300

    Mobile header.

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <>
+            <header className="flex flex-col justify-center items-center bg-black mb-8 ">
+                <h1 className="flex flex-row text-whitish font-black text-3xl mb-2">
+                    <span>🃏</span>Find a Pair</h1>
+                <p className="text-grayish font-semibold">Match all 8 pairs to win</p>
+            </header>
+        </>
+    )
+}
+
+export default Header;
